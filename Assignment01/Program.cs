94c8b032bc8d8dff941581182ab5dfe73b04b61f
user <user@DESKTOP-ERSTO5F>
2025-01-06 03:20:18 +0200

    Add project files.

---
+++ Assignment01/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace Assignment01
+{
+    class point
+    {
+        public int x;
+        public int y;
+    }
+    internal class Program
+    {
+
+        static void Main()
+        {
+
+            #region Q1
+
+            //Console.WriteLine("Enter Number");
+            //int Num = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Number : " + Num);
+            #endregion
+
+            #region Q2
+
+            //Console.WriteLine(value:"Enter String");
+            //int Num = Convert.ToInt32 (Console.ReadLine());
+
+            //Console.WriteLine(Num);
+
+            //it cant be done because its not in the correct format
+            #endregion
+
+            #region Q3
+
+            //Console.WriteLine(value: "Enter Number");
+            //double Num1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine(value: "Enter Number");
+            //double Num2 = double.Parse(Console.ReadLine());
+
+            //double sum = Num1 + Num2;
+            //Console.WriteLine(sum);
+
+            // it will sum the two variables
+            #endregion
+
+
+            #region Q4
+
+            //string str = "The Matrix";
+            //string substr = str.Substring(0, 3);
+            //string substr2 = str.Substring(4);
+            //Console.WriteLine(substr2);
+            #endregion
+
+            #region Q5
+
+            //double x = 1.2;
+            //double y = x;
+            //x = 1.5;
+            //Console.WriteLine(x); // 1.5
+            //Console.WriteLine(y); // 1.2
+
+            // it cant see what modification after its assigned to one variable
+            #endregion
+
+            #region Q6
+
+            //point p1 = new point();
+            //point p2 = new point();
+
+            //p1.x = 4;
+            //p1.y = 5;
+
+            //p2 = p1;
+
+            //p2.x = 6;
+            //p1.y = 9;
+
+            //Console.WriteLine($"{p1.x},{p1.y}"); // // $" to type string mixed with {variable}"
+            //Console.WriteLine($"{p2.x},{p2.y}"); // 
+            //it will change because its reference type saves the address no matter when modifications occured
+            #endregion
+
+            #region Q7
+
+            //Console.WriteLine("enter 1st string");
+            //string str1 = Console.ReadLine();
+
+            //Console.WriteLine("enter 2nd string");
+            //string str2 = Console.ReadLine();
+
+            //Console.WriteLine(str1 + " " + str2);
+            #endregion
+
+            #region Q8
+
+            //int pamount = 5, rate = 6, time = 10;
+            //int intrst = (pamount * rate * time) / 100;
+
+            //Console.WriteLine(intrst);
+            #endregion
+
+            #region Q9
+
+            //double weight = 70, height = 1.8;
+            //double bmi = (weight) / (height * height);
+
+            //Console.WriteLine(bmi);
+            #endregion
+
+            #region Q10
+
+            //while (true)
+            //{
+            //    Console.Write("Enter The Degree Of Temp.: ");
+            //    int temp = int.Parse(Console.ReadLine());
+            //    string output = (temp < 10 ? "Just Cold" : temp > 30 ? "Just Hot" : "Just Good");
+            //    Console.WriteLine(output);
+            //}
+            #endregion
+
+            #region Q11
+
+            //Console.Write("Enter Day: ");
+            //int day = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Month: ");
+            //int month = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Year: ");
+            //int year = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Today's date : {day} , {month} , {year}");
+            //Console.WriteLine($"Today's date : {day} / {month} / {year}");
+            //Console.WriteLine($"Today's date : {day} - {month} - {year}");
+            #endregion
+
+            #region Q12
+
+            //DateTime date = new DateTime(2024, 6, 14);
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+            //// the output > The event is on  06/ 14/2024 c)
+            // MM/dd/yyyy is defined in c# // though DateTime is (year, month, day)
+            #endregion
+
+            #region Q13
+
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //Console.WriteLine(d);
+            //// ! = not (opposite) // so it will be assigned to 1 because its true f)
+            //// if ! is removed it will be assigned to 0 (false)
+            #endregion
+
+            #region Q14
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+            // output > 6 1 d)
+            // not 6.5 because its int division
+            #endregion
+
+            #region Q15
+
+            //int num = 1, z = 5;
+
+            //if ((num >= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+            //// output >> 77
+            #endregion
+        }
+
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,278 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        // entry point
+        static void Main(string[] args)
+        {
+            #region var_dec
+            //// variable declaration
+            //// datatype variablename;
+
+
+            //int Number; // Declare Variable
+            //// Allocate UnInitialized 4 Bytes At Memory
+
+            ////Console.WriteLine(Number); // Invalid
+
+            //Number = 5;
+            ////variable
+            //// 1. Name      :Number
+            //// 2. Datatype: :int 
+            //// 3. Size      :4 Bytes
+            //// 4. Value     :5 
+            //// 5. Address : 0xF2A
+
+            ////Console.WriteLine(Number); // 5
+
+            ////int Number = 5; // Declaration And Initialization For The Number
+
+            ////Console.WriteLine(Number);
+
+            ////int Number01;
+            ////int Number02;
+            ////int Number03;
+
+            ////int Number01 = 2, Number02 = 3, Number03,
+
+            ////Console.WriteLine(Number01); //2
+            ////Number01 = 7;
+            ////Console.WriteLine(Number01);
+            ////Console.WriteLine(Number03); 
+            #endregion
+
+            #region datatype
+            //// Datatype (Value Type): Stacki
+
+            //// Declare Variable
+
+            //int Number; // Value Type
+
+            //// Allocate UnInitialized 4 Bytes At Stack
+
+            //// int : C# Keyword
+
+            //// BCL: Int32
+
+            //Int32 Number02;
+
+            //Number = 12;
+
+            ////Console.WriteLine(Number02);
+            #endregion
+
+            #region point
+            //Point P01; //reference
+            //// declare reference from 'Point'
+            //// 8 bytes will be allocated at stack
+            //// 0 bytes will be allocated at heap
+
+            //Console.WriteLine(P01);
+
+            //Console.WriteLine(P01.X); // invalid
+
+            //P01 = new Point();
+            //// new
+            //// 1. allocate place at heap
+            //// 2. initialize the allocation with default value
+            //// 3. call user-define construtor if exist
+            //// 4. assign the object address to the reference 'P01'
+
+            //Console.WriteLine(P01.X);// 0
+            //Console.WriteLine(P01.Y);// 0
+            //Console.WriteLine(P01);  // namespace
+
+
+
+            //Point P02 = new Point();
+
+            //P02.X = 12;
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P02.X);
+
+            //P01 = P02;
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P02.X);
+            #endregion
+
+
+            #region object
+            //object 001;
+            //// declare for reference from type 'object'
+            //// 001: can refer to instance from type 'object' or any type inherited from object
+
+
+            //001 = new object();
+            //001 = 1;
+            //001 = 'A';
+            //001 = 1.2;
+            //001 = true;
+            //001 = new Point();
+
+            //Console.WriteLine(001.GetHashCode()); // id : indication for the address
+
+            //Console.WriteLine(001); // namespace
+
+            //Point P01 = new Point();
+            //Console.WriteLine(P01.GetHashCode()); // id 
+            #endregion
+
+            #region casting
+            //casting : converting datatype
+            // 1. implicit casting
+
+            //int X = 5;
+
+            //double Y = /*(double)*/ X; // implicit casting (safe)
+
+            //Console.WriteLine(Y);
+
+
+            //// 2. explicit casting
+
+            //double X = 2.5;
+
+            //int Y = (int) X; // explicit cast (unsafe)
+            //Console.WriteLine(Y);
+
+
+            //long X = 111111111111111111111111;
+
+            //int Y = (int) X; // explicit
+            //Console.WriteLine(Y);
+
+            //Console.WriteLine(int.MaxValue); 
+            #endregion
+
+            #region convert,parse,tryparse
+            // parse : function
+
+            //Console.WriteLine(value:"Enter your Age: ");
+            //int Age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(value: "Enter your GPA: ");
+            //double GPA = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Age : " + Age);
+            //Console.WriteLine("GPA : " + GPA);
+
+            // convert : class
+
+            //Console.WriteLine(value: "Enter your Age: ");
+            //int Age = Convert.ToInt32(Console.ReadLine());
+
+
+            ////Console.WriteLine(value: "Enter your GPA: ");
+            ////double GPA = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Age : " + Age);
+            ////Console.WriteLine("GPA : " + GPA);
+
+
+            // tryparse
+
+            //Console.WriteLine(value: "Enter your Age: ");
+
+            //int Age;
+            //bool Flag = int.TryParse(Console.ReadLine(), out Age);
+
+            ////Console.WriteLine(value: "Enter your GPA: ");
+            ////double GPA = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Age : " + Age);
+            ////Console.WriteLine("GPA : " + GPA); 
+            #endregion
+
+
+            #region fractions & discrads
+            //double X = 1.5;
+            //float Y = 1.5F;
+
+            //decimal Z = 1.5m;
+
+
+            //    int X = 1_000_000_000; // Discard
+
+            //    Console.WriteLine(X);
+            //}
+            #endregion
+
+            #region operators
+            //1. Unary Operators -> Works On One Operand (Variable) ++ 
+            //int X = 10;
+
+            // ++
+            //Prefix [Increment then print]
+            //Console.WriteLine(++X); // 11 
+            //PostFix [Print then Increment]
+            //Console.WriteLine(X++); // 10 
+            //Console.WriteLine(X); // 11
+
+
+            //
+            //Prefix [decrement then print]
+            //Console.WriteLine(--X); // 9
+            //PostFix [Print then decrement] 
+            //Console.WriteLine(X--); // 10 
+            //Console.WriteLine(X); // 9
+
+            //=====================================
+
+            //2. Binary Operators => Works on Two Operands (Variables)
+            // + = / * %
+            //int Sum, Mul, Sub, Mod;
+            //int Number01 = 2, Number02 = 6;
+
+            //Sum = Number01 + Number02; // 8 
+            //Mul = Number01 Number02; // 12
+            //Sub = Number01 Number02; // -4 
+            //Mod Number01 % Number02; // 2 
+            // 23 % 3 = 2
+
+            //=======================================
+            //3. Assignment Operators
+
+            //int X; //X = 4;
+            //X += 2; // X = X + 2 // 6 
+            //X -= 2; // X = X - 2
+            //X *= 2; // X = X * 2
+            //X /= 2; // X = X / 2
+            //X %= 2; // X = X % 2
+
+
+
+
+            //4. Relational Operators
+
+
+            //int X = 6, Y = 5;
+            //Console.WriteLine(X == Y);
+            //Console.WriteLine(X != Y);
+            //Console.WriteLine(X > Y);
+            //Console.WriteLine(X < Y);
+            //Console.WriteLine(X >= Y);
+            //Console.WriteLine(x <= Y);
+
+            //5. Logical Operators [AND, OR, NOT]
+            //Console.WriteLine(!false); // ! -> Not
+            //Console.WriteLine(false && true); // && -> AND Short Circuit 
+            //Console.WriteLine(false || true); // || -> OR Short Circuit
+
+            //6. Bitwise Operators
+            //Console.WriteLine(!false); // ! -> Not
+            //Console.WriteLine(false && true); // && -> AND Long Circuit
+            //Console.WriteLine(false || true); // || -> OR Long Circuit
+
+            //7. Ternary Operator 
+
+
+            //bool flag = 4 > 2;
+            //Console.WriteLine(flag);
+            //string flag = 4 < 2 ? "Correct" : "Not Correct";
+            //Console.WriteLine(flag); 
+            #endregion
+
+        }
+    }
